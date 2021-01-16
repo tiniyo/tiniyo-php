0bc826b241effed1f873dfeccfb037737ddbd46b
Support Tiniyo <support@tiniyo.com>
2021-01-16 19:40:31 +0530

    composer update

--- src/Rest/Client.php
+++ src/Rest/Client.php
@@ -2,10 +2,12 @@
 namespace Tiniyo\Rest;
 
 class Client extends \Twilio\Rest\Client {
+  
   public function __construct($accountSid, $token) {
     $httpClient = isset($options["httpClient"]) ? $options["httpClient"] : null;
     parent::__construct($accountSid, $token, $accountSid, null, $httpClient, null);
-    $domain = "https://api.tiniyo.com"
+    $domain = "https://api.tiniyo.com";
     $this->_api = new Api($this, $domain);
   }
+
 }
