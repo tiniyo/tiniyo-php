158b4c7906256bd7f1d1f72f078508b33734c583
Support Tiniyo <support@tiniyo.com>
2021-01-16 18:04:16 +0530

    fixing typo

--- src/Rest/Client.php
+++ src/Rest/Client.php
@@ -2,8 +2,7 @@
 namespace Tiniyo\Rest;
 
 class Client extends \Twilio\Rest\Client {
-
-  public function __construct($accountSid, $token)) {
+  public function __construct($accountSid, $token) {
     $httpClient = isset($options["httpClient"]) ? $options["httpClient"] : null;
     parent::__construct($accountSid, $token, $accountSid, null, $httpClient, null);
     $domain = "https://api.tiniyo.com"
